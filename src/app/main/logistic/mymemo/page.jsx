5e23094bd86f64c13e42b66cc09457e803474e56
ananda.riza <rizapratomo@graduate.utm.my>
2024-03-21 21:32:13 +0700

    changes in My memo and New Memo pages, also added new components corresponding with newly added pages

--- src/app/main/logistic/mymemo/page.jsx
+++ src/app/main/logistic/mymemo/page.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import LogisticTable from "@/components/logistic_components/logistic_table";
 
 const page = () => {
-  return (
-    <div>my memo</div>
-  )
-}
+ 
 
-export default page
+  return (
+    <>
+      <div className="flex-grow justify-center items-center min-h-screen">
+          <div>
+            <div className="text-[#0066AE] font-semibold ml-10 mt-4">
+              My Memo
+            </div>
+            <div>
+              <span className="flex text-[#0066AE] ml-10 mt-2">
+                <div className="text-sm breadcrumbs"> 
+                  <ul>
+                    <li>
+                      <a>Home</a>
+                    </li>
+                    <li>
+                      <a>Documents</a>
+                    </li>
+                    <li>Add Document</li>
+                  </ul>
+                </div>
+              </span>
+            </div>
+            <div className="px-10 grid grid-cols-2 gap-3 mt-10 w-full"></div>
+            <LogisticTable />
+          </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
--- src/app/main/logistic/page.jsx
+++ src/app/main/logistic/page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LogisticTable from "@/components/logistic_components/LogisticTable";
+import LogisticTable from "@/components/logistic_components/logistic_table";
 
 const page = () => {
   // const generateBreadcrumbs = () => {
@@ -18,11 +18,11 @@ const page = () => {
       <div className="flex-grow justify-center items-center min-h-screen">
           <div>
             <div className="text-[#0066AE] font-semibold ml-10 mt-4">
-              Dashboard
+              All Memo
             </div>
             <div>
               <span className="flex text-[#0066AE] ml-10 mt-2">
-                <div className="text-sm breadcrumbs">
+                <div className="text-sm breadcrumbs"> 
                   <ul>
                     <li>
                       <a>Home</a>
