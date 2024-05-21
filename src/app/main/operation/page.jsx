ee98cc90de2889236bf08c00beea679a523e993e
Ridhwankiki <80313535+Ridhwankiki@users.noreply.github.com>
2024-05-21 09:18:19 +0700

    housekeeping network_card and network page

--- src/app/main/operation/page.jsx
+++ src/app/main/operation/page.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Card from "@/components/network/network_card";
 
 const data = [
-  { nama_sistem: "Hard Disk 1", keterangan: "Hard Disk Wisma 1", thresehold_1: "500 MB", thresehold_2: "500 MB", thresehold_3: "500 MB"},
-  { nama_sistem: "Hard Disk 2", keterangan: "Hard Disk Wisma 2", thresehold_1: "1000 MB", thresehold_2: "500 MB", thresehold_3: "500 MB"},
-  { nama_sistem: "Hard Disk 3", keterangan: "Hard Disk Wisma 3", thresehold_1: "1000 MB", thresehold_2: "500 MB", thresehold_3: "500 MB"},
-  { nama_sistem: "Hard Disk 4", keterangan: "Hard Disk Wisma 4", thresehold_1: "1000 MB", thresehold_2: "500 MB", thresehold_3: "500 MB"},
+  { nama_sistem: "Hard Disk 1", keterangan: "Hard Disk Wisma 1", threshold_1: "500 MB", threshold_2: "500 MB", threshold_3: "500 MB"},
+  { nama_sistem: "Hard Disk 2", keterangan: "Hard Disk Wisma 2", threshold_1: "1000 MB", threshold_2: "500 MB", threshold_3: "500 MB"},
+  { nama_sistem: "Hard Disk 3", keterangan: "Hard Disk Wisma 3", threshold_1: "1000 MB", threshold_2: "500 MB", threshold_3: "500 MB"},
+  { nama_sistem: "Hard Disk 4", keterangan: "Hard Disk Wisma 4", threshold_1: "1000 MB", threshold_2: "500 MB", threshold_3: "500 MB"},
 ];
 
 
@@ -18,7 +18,7 @@ const page = () => {
             <div className="px-10 grid grid-cols-2 gap-3 mt-10 w-full">
               {data.map((item, index) => (
                 <div key={index} className="w-full">
-                  <Card nama_sistem={item.nama_sistem} keterangan={item.keterangan} thresehold_1={item.thresehold_1} />
+                  <Card nama_sistem={item.nama_sistem} keterangan={item.keterangan} threshold_1={item.threshold_1} threshold_2={item.threshold_2} threshold_3={item.threshold_3} />
                 </div>
               ))}
             </div>
