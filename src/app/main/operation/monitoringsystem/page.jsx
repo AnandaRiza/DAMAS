5b63957c867cb1916ae24794109104bd2f09105b
Ridhwankiki <80313535+Ridhwankiki@users.noreply.github.com>
2024-05-21 11:56:59 +0700

    houskeeping monitoring system page

--- src/app/main/operation/monitoringsystem/page.jsx
+++ src/app/main/operation/monitoringsystem/page.jsx
@@ -1,28 +1,24 @@
 import React from "react";
-import Card from "@/components/Card";
+import Card from "@/components/network/network_card";
 
 const data = [
-  { name: "riza", age: 20 },
-  { name: "ridhwan", age: 24 },
-  { name: "maya", age: 17 },
-  { name: "aula", age: 17 },
+  { nama_sistem: "Hard Disk 1", keterangan: "Hard Disk Wisma 1", threshold_1: "500 MB", threshold_2: "500 MB", threshold_3: "500 MB"},
+  { nama_sistem: "Hard Disk 2", keterangan: "Hard Disk Wisma 2", threshold_1: "1000 MB", threshold_2: "500 MB", threshold_3: "500 MB"},
+  { nama_sistem: "Hard Disk 3", keterangan: "Hard Disk Wisma 3", threshold_1: "1000 MB", threshold_2: "500 MB", threshold_3: "500 MB"},
+  { nama_sistem: "Hard Disk 4", keterangan: "Hard Disk Wisma 4", threshold_1: "1000 MB", threshold_2: "500 MB", threshold_3: "500 MB"},
 ];
+
 
 const page = () => {
   return (
       <div className="flex-grow justify-center items-center min-h-screen">
             <div className="text-[#0066AE] font-semibold ml-10 mt-4">
-              Dashboard
-            </div>
-            <div>
-              <span className="flex text-[#0066AE] ml-10 mt-2">
-                Working & Managing Together
-              </span>
+              Monitoring Sistem
             </div>
             <div className="px-10 grid grid-cols-2 gap-3 mt-10 w-full">
               {data.map((item, index) => (
                 <div key={index} className="w-full">
-                  <Card name={item.name} age={item.age} />
+                  <Card nama_sistem={item.nama_sistem} keterangan={item.keterangan} threshold_1={item.threshold_1} threshold_2={item.threshold_2} threshold_3={item.threshold_3} />
                 </div>
               ))}
             </div>
