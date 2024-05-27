fc2ecebc1c55c4001873b180c0275a4df8b32734
Ananda Riza <87215017+AnandaRiza@users.noreply.github.com>
2024-05-27 11:08:06 +0700

    added getAllMemo Function and Create new memo

--- src/app/main/logistic/createnewmemo/page.jsx
+++ src/app/main/logistic/createnewmemo/page.jsx
@@ -1,18 +1,31 @@
-import React from "react";
 import MemoForm from "@/components/logistic_components/logistic_memo_form";
-import LogisticBreadcrumbs from "@/components/logistic_components/logistic_breadcrumbs";
+import React from 'react'
 
 const page = () => {
   return (
     <>
-      <div className="flex-grow justify-center items-center min-h-screenn">
-        <div className="w-full max-w-4xl">
+      <div className="flex-grow justify-center items-center min-h-screen">
+        <div>
           <div className="text-[#0066AE] font-semibold ml-10 mt-4">
             Create New Memo
           </div>
-          <LogisticBreadcrumbs />
-          <div className="px-10 mt-10">
-            <MemoForm />
+          <div>
+            <span className="flex text-[#0066AE] ml-10 mt-2">
+              <div className="text-sm breadcrumbs">
+                <ul>
+                  <li>
+                    <a>Home</a>
+                  </li>
+                  <li>
+                    <a>Memo</a>
+                  </li>
+                  <li>Add Memo</li>
+                </ul>
+              </div>
+            </span>
+          </div>
+          <div className="px-10 grid grid-cols-2 gap-3 mt-10 w-full">
+            <MemoForm/>
           </div>
         </div>
       </div>
@@ -20,4 +33,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default page
