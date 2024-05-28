31570f2fedfbcc12ad9f93a2c7b8940ecad8e70b
Ananda Riza <87215017+AnandaRiza@users.noreply.github.com>
2024-05-28 14:14:18 +0700

    housekeeping

--- src/components/logistic_components/logistic_edit_memo_form.jsx
+++ src/components/logistic_components/logistic_edit_memo_form.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const EditMemoForm = ({ memoId }) => {
+    
     const [formData, setFormData] = useState({
         memo_num: "",
         memo_perihal: "",
