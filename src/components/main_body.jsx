6b8739192b04659dd0a809323c1616969c036228
Ridhwankiki <80313535+Ridhwankiki@users.noreply.github.com>
2024-07-10 16:57:08 +0700

    housekeeping navbar, main page (not done yet)

--- src/components/main_body.jsx
+++ src/components/main_body.jsx
@@ -1,56 +1,42 @@
-'use client';
+"use client";
 
 import axios from "axios";
 import React, { useState } from "react";
 import { DiVim } from "react-icons/di";
 
 const Page = () => {
+  return (
+    <div className="flex-grow justify-center items-center min-h-screen rounded-xl mt-1">
+      <div className="flex w-grow">
+        <div className="grid h-[330px] w-[90px] flex-grow card bg-white rounded-box mt-4 p-1 mr-2"
+        // style={{ background: 'linear-gradient(135deg, rgba(255, 255, 255, 1), rgba(0, 166, 180, 0.3))' }}
+        >
+          <div className="h-[50px] w-[770px] rounded-box shadow-sm">
+          <h1 className="font-bold p-2 mt-1 items-center justify-center">DEVELOPMENT</h1>
+          <p className="p-4">Monitoring all STL Project From MBS</p>
+          </div>
+        </div>
 
-return (
-    <div className="flex-grow bg-[#FFFFFF] justify-center items-center min-h-screen bg-white rounded-xl mt-4">
+        {/* <div className="divider divider-horizontal"></div> */}
 
-<div className="flex w-grow">
+        <div className="grid h-[330px] w-[90px] flex-grow card bg-white rounded-box mt-4 p-1">
+          <h1 className="font-bold p-2">PPO</h1>
+        </div>
+      </div>
 
-  <div className="grid h-20 w-20 flex-grow card bg-base-300 rounded-box place-items-center mt-4 ml-4 p-1">
-    DEVELOPMENT
+      <div className="flex w-grow mt-1">
+        <div className="grid h-[330px] w-[90px] flex-grow card bg-white rounded-box mt-2 p-1 mr-2 font-bold">
+        <h1 className="font-bold p-2">OPERATION</h1>
+        </div>
+
+        {/* <div className="divider divider-horizontal"></div> */}
+
+        <div className="grid h-[330px] w-[90px] flex-grow card bg-white rounded-box mt-2 p-1 font-bold">
+        <h1 className="font-bold p-2">LOGISTIC</h1>
+        </div>
+      </div>
     </div>
-
-  <div className="divider divider-horizontal"></div>
-
-  <div className="grid h-20 w-20 flex-grow card bg-base-300 rounded-box place-items-center mt-4 mr-4 p-1">
-    PPO
-    </div>
-
-</div>
-
-<div className="flex w-grow mt-2">
-
-  <div className="grid h-60 w-20 flex-grow card bg-base-300 rounded-box place-items-center mt-4 ml-4 p-1">
-    <div className="font-bold">
-    OPERATION
-    </div>
-    <div>
-        
-    </div>
-
-    <div className="">
-        <button className="btn btn-success"> 123</button>
-        <button className="btn btn-success ml-2"> 123</button>
-        <button className="btn btn-success ml-2"> 123</button>
-    </div>
-    </div>
-
-  <div className="divider divider-horizontal"></div>
-
-  <div className="grid h-20 w-20 flex-grow card bg-base-300 rounded-box place-items-center mt-4 mr-4 p-1">
-    LOGISTIC
-    </div>
-
-</div>
-
-    </div>
-    
-);
+  );
 };
 
 export default Page;
